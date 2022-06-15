2830340a0a9e628095df80d2b5821a2c7805c239
Derssa <youssefmoinou@gmail.com>
2022-06-15 15:59:49 +0100

    fixing analytic

--- pages/_app.js
+++ pages/_app.js
@@ -6,25 +6,23 @@ import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-      />
-      <Script
-        id="gtag"
-        strategy="lazyOnload"
-      >{`window.dataLayer = window.dataLayer || [];
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          id="gtag"
+          strategy="lazyOnload"
+        >{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', '${process.env.GOOGLE_ANALYTICS}');`}</Script>
-      <SessionProvider session={session}>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </SessionProvider>
-    </>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
   );
 }
 
