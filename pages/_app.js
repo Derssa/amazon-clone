2a97e12ecc56652f362eab62a1c9466882a9425b
Derssa <youssefmoinou@gmail.com>
2022-06-15 15:23:28 +0100

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,26 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </SessionProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">{`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.GOOGLE_ANALYTICS});`}</Script>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </SessionProvider>
+    </>
   );
 }
 
