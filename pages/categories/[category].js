366ed2716360275a229018e73b57371e52ed9ee2
Derssa <youssefmoinou@gmail.com>
2022-11-01 14:21:20 +0100

    addIcon

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -21,13 +21,13 @@ export default function Name({ products }) {
       <Head>
         <title>{category}</title>
         <meta name="description" content={category} />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="theme-color" content="#ffffff">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
 
       {/* header */}
