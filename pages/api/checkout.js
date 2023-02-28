1ec20633d742bde404dd0065f55e2363e930e263
Derssa <youssefmoinou@gmail.com>
2023-02-28 20:19:47 +0100

    test check1

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,3 @@
-const nodemailer = require("nodemailer");
-
 export default async function checkout(req, res) {
   try {
     const client = (await import("../../sanity")).client;
@@ -11,23 +9,6 @@ export default async function checkout(req, res) {
     }
 
     await client.create(order);
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PWD,
-      },
-    });
-
-    const mailOptions = {
-      from: process.env.EMAIL,
-      to: process.env.EMAIL,
-      subject: "New Order",
-      text: `${order.clientName} ,${order.amount} EUR`,
-    };
-
-    await transporter.sendMail(mailOptions);
     res.status(200).json({ msg: "opération réussie" });
   } catch (err) {
     return res.status(500).json({ msg: "La demande ne peut pas être ajoutée" });
